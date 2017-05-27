56b72492932d7e02d4cdf82af7fc96410c2c2424
Chase Miller <chmille4@gmail.com>
2017-05-27 08:58:23 -0400

    make file script local path

--- server/server.js
+++ server/server.js
@@ -26,15 +26,10 @@ app.get('/bamstatsalive', function (req, res) {
   bamstatsalive.stdout.pipe(res);
 })
 
-// app.post('/clientbai', function(req, res) {
-//     var baiReadDepth = spawn('/Users/chase/Code/minion/bin/bamReadDepther');
-//     baiReadDepth.stdout.pipe(res);
-//     req.pipe(baiReadDepth.stdin);
-// });
 
 app.post('/clientbam', function(req, res) {
     var args = ["-u", req.query.u, "-k", req.query.k, "-r", req.query.r, '-s']
-    var bamstatsalive = spawn('/Users/chase/Code/playground/monolith_test/bamstatsalive.sh', args);
+    var bamstatsalive = spawn(path.join(__dirname,'/scripts/bamstatsalive.sh'), args);
     bamstatsalive.stdout.on('error', function(e) { console.log('out = ' + e)})
     bamstatsalive.stdin.on('error', function(e) { console.log(e)})
     req.pipe(bamstatsalive.stdin);
